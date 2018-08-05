334edc3ccc676df681bc68f8039452ca653771d2
Cyril CATOIRE <ccatoire@e3r4p19.42.fr>
2018-08-05 18:45:44 +0200

    init/script corrigeais

---
+++ minishell/includes/minishell.h
@@ -0,0 +1,8 @@
+#ifndef		MINISHELL_H
+# define	MINISHELL_H
+
+#include "libft.h"
+
+int	minishell();
+
+#endif
---
+++ minishell/src/main.c
@@ -0,0 +1,13 @@
+#include "minishell.h"
+
+
+
+int	main(/*int ac, char **av*/)
+{
+//	if (ac != 2)
+//		return (ft_error(USE, FAIL));
+
+	minishell();
+
+	return (OK);
+}
