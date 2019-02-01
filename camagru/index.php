3ca095274a93b5dc39d8bfe337376b924b274478
Cyril CATOIRE <ccatoire@e3r4p19.42.fr>
2019-02-01 18:46:15 +0100

    camagruuuuu

---
+++ camagru/index.php
@@ -0,0 +1,57 @@
+<?PHP
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css">
+        <!--[if lte IE 8]>
+            <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-old-ie-min.css">
+        <![endif]-->
+        <!--[if gt IE 8]><!-->
+            <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
+        <!--<![endif]-->
+
+        <link rel="stylesheet" href="style/index.css">
+
+        <title>camaGRU</title>
+    </head>
+    <body>
+
+        <header id="head" class="pure-g">
+
+
+            <div id="home-box" class="pure-menu pure-menu-horizontal pure-u-1 pure-u-md-1-5 pure-u-sm-2-5">
+                <ul class="pure-menu-list">
+                    <li class="pure-menu-item"><a href="index.php" class="pure-menu-link">Home</a></li>
+                    <li class="pure-menu-item"><a href="gallery.php" class="pure-menu-link">Gallery</a></li>
+                </ul>
+            </div>
+
+            <div class="pure-u-1 pure-u-md-3-5 pure-u-sm-1-5"><p>CamaGruuuuuuu</p> </div>
+
+            <div id="sign-box" class="pure-menu pure-menu-horizontal pure-u-1 pure-u-md-1-5 pure-u-sm-2-5">
+                <ul class="pure-menu-list">
+                    <li class="pure-menu-item"><a href="sign-in.php" class="pure-menu-link">Sign in</a></li>
+                    <li class="pure-menu-item"><a href="sign-up.php" class="pure-menu-link">Sign up</a></li>
+                </ul>
+            </div>
+
+
+        </header>
+
+
+        <!-- <section>
+
+        </section>
+
+
+        <footer>
+        
+        </footer> -->
+    </body>
+</html>
